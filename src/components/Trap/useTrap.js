4ec0780557aebd2ab074d2c5fb0b1cf6d50b8c23
sag1v <sagiv.bengiat@gmail.com>
2019-03-09 10:00:21 +0200

    improve useEffect with "eslint-plugin-react-hooks"

--- src/components/Trap/useTrap.js
+++ src/components/Trap/useTrap.js
@@ -14,6 +14,34 @@ const useTrap = ({ event, on, off, preventDefault }) => {
 
   useEffect(
     () => {
+      const preventDefaultHelper = (e, nextTrapped) => {
+        if (trapped !== nextTrapped && preventDefault) {
+          e.preventDefault();
+        }
+      };
+
+      const handleEvent = e => {
+        const trapped = ref.current.contains(e.target);
+        preventDefaultHelper(e, trapped);
+        setTrapped(trapped);
+      };
+
+      const handleFocusEvent = e => {
+        const trapped = ref.current.contains(e.target);
+        preventDefaultHelper(e, trapped);
+        if (trapped) {
+          setTrapped(true);
+        }
+      };
+
+      const handleBlurEvent = e => {
+        const trapped = ref.current.contains(e.target);
+        preventDefaultHelper(e, trapped);
+        if (!trapped) {
+          setTrapped(false);
+        }
+      };
+
       if (on && off) {
         // we got seperate events for focus and blur
         asArray(on).forEach(evt => {
@@ -54,34 +82,6 @@ const useTrap = ({ event, on, off, preventDefault }) => {
     // re-run on ref event, on, off, preventDefault changes
     [event, on, off, preventDefault, trapped]
   );
-
-  const preventDefaultHelper = (e, nextTrapped) => {
-    if (trapped !== nextTrapped && preventDefault) {
-      e.preventDefault();
-    }
-  };
-
-  const handleEvent = e => {
-    const trapped = ref.current.contains(e.target);
-    preventDefaultHelper(e, trapped);
-    setTrapped(trapped);
-  };
-
-  const handleFocusEvent = e => {
-    const trapped = ref.current.contains(e.target);
-    preventDefaultHelper(e, trapped);
-    if (trapped) {
-      setTrapped(true);
-    }
-  };
-
-  const handleBlurEvent = e => {
-    const trapped = ref.current.contains(e.target);
-    preventDefaultHelper(e, trapped);
-    if (!trapped) {
-      setTrapped(false);
-    }
-  };
 
   return [trapped, setRef];
 };
